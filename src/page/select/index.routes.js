2ed36e4130532ae40b356c7018695874f56ba41a
陈伟源 <hzchenweiyuan@corp.netease.com>
2018-12-05 15:38:26 +0800

    select

---
+++ src/page/select/index.routes.js
@@ -0,0 +1,5 @@
+import Index from './index.vue';
+export default {
+    path: 'select',
+    component: Index,
+}
---
+++ src/page/war/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="war-wrapper">
+        <div class="left">left</div>
+        <div class="right">right</div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+<style>
+.war-wrapper {
+    width: 100%;
+}
+.left {
+    float: left;
+    width: 50%;
+    box-sizing: border-box;
+    padding: 20px;
+    border: 1px solid red;
+    /* background: #f66; */
+}
+.right {
+    float: right;
+    width: 50%;
+    box-sizing: border-box;
+    padding: 20px;
+    border: 1px solid blue;
+    /* background: #66f; */
+}
+</style>
