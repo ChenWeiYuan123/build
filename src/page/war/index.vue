41570d084b90563521732d2a1ead3f94a4aa2add
陈伟源 <hzchenweiyuan@corp.netease.com>
2018-12-06 21:48:41 +0800

    add war

--- src/page/war/index.vue
+++ src/page/war/index.vue
@@ -1,16 +1,115 @@
 <template>
     <div class="war-wrapper">
-        <div class="left">left</div>
-        <div class="right">right</div>
+        left:
+        x:<input v-model="left.x" type="text"/>y:<input v-model="left.y" type="text"/>
+        
+        right:
+        x:<input v-model="right.x" type="text"/>y:<input v-model="right.y" type="text"/>
+        <button @click="save">save</button>
+        <div class="toolWrapper">
+            <div class="tool" draggable="true" @dragstart="dragstart('plane', $event)">
+                plane
+            </div>
+        </div>
+        <div>
+            <div class="left">
+                <div v-for="(row, index1) in left.data" class="row">
+                    <div class="item" v-if="Array.isArray(row)" v-for="(item, index2) in row">
+                        <input v-if="isEdit('left', index1, index2)" v-model="value" @blur="blur" type="text"/>
+                        <div v-else class="content" @click="edit('left', index1, index2)" @dragover.prevent @drop="drop('left', index1, index2, $event)">{{item}}</div>
+                    </div>
+                </div>     
+            </div>
+            <div class="right">
+                <div v-for="(row, index1) in right.data" class="row">
+                    <div class="item" v-if="Array.isArray(row)" v-for="(item, index2) in row">
+                        <input v-if="isEdit('right', index1, index2)" v-model="value" @blur="blur" type="text"/>
+                        <div v-else class="content" @click="edit('right', index1, index2)" @dragover.prevent @drop="drop('right', index1, index2, $event)">{{item}}</div>
+                    </div>
+                </div>    
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-
+            editData: {
+                type: '',
+                x: undefined,
+                y: undefined,
+            },
+            left: {
+                x: 5,
+                y: 5,
+                data: [],
+            },
+            right: {
+                x: 5,
+                y: 5,
+                data: [],
+            },
+            value: '',
         }
     },
+    created() {
+        this.init();
+        this.$watch(() => this.left.x+this.left.y, () => {
+            this.createItems(this.left)
+        });
+        this.$watch(() => this.right.x+this.right.y, () => {
+            this.createItems(this.right)
+        });
+    },
+    methods: {
+        drop(type, x, y, ev){
+            ev.preventDefault();
+            const value = ev.dataTransfer.getData("tool");
+            this.setData(type, x, y, value);
+        },
+        dragstart(tool, ev) {
+            ev.dataTransfer.setData("tool", tool);
+        },
+        edit(type, x, y) {
+            this.editData.type = type;
+            this.editData.x = x;
+            this.editData.y = y;
+        },
+        blur(e) {
+            this[this.editData.type].data[this.editData.x][this.editData.y] = this.value;
+            this.resetEdit();
+        },
+        setData(type, x, y, value) {
+            this[type].data[x][y] = value;
+            this.$forceUpdate();
+        },
+        resetEdit() {
+            this.value = '';
+            this.editData = {
+                type: '',
+                x: undefined,
+                y: undefined,
+            };
+        },
+        isEdit(type, x, y){
+            return type === this.editData.type && x === this.editData.x && y === this.editData.y;
+        },
+        init() {
+            this.createItems(this.left);
+            this.createItems(this.right);
+        },
+        createItems(block) {
+            block.data = Array.from({length: block.y}, (v, i) => i);
+            block.data.forEach((item, index) => {
+                block.data[index] = Array.from({length: block.x}, (v, i) => 0);
+            });
+        },
+        save() {
+            const data = { left: this.left, right: this.right};
+            localStorage.setItem('war', JSON.stringify(data));
+        }
+    }
 }
 </script>
 <style>
@@ -32,5 +131,29 @@ export default {
     padding: 20px;
     border: 1px solid blue;
     /* background: #66f; */
+}
+.row {
+    display: flex;
+    justify-content: space-around;
+}
+.item {
+    float: left;
+    margin: 5px;
+    box-sizing: content-box;
+    cursor: pointer;
+}
+.content{
+    padding: 30px;
+    background: #faa;
+}
+.tool{
+    background: #aff;
+    width: 50px;
+    padding: 10px;
+}
+.toolWrapper{ 
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #333;
 }
 </style>
